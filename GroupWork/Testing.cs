dd3a64b2a8e0b3fa362b9c56c0851b7a534923e5
Lynda nneka <anaedobelyn001@gmail.com>
2023-03-27 00:14:37 +0100

    test commit

---
+++ GroupWork/Testing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GroupWork
+{
+    public static class Testing
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello Team");
+        }
+    }
+}
+
